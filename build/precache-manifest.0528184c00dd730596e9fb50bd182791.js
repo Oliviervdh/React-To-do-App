c7e1381bfcb18cd16e434df01e1579c7a6d04e4c
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-08-07 15:48:14 +0200

    Adding Github Pages

---
+++ build/precache-manifest.0528184c00dd730596e9fb50bd182791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808e2b132244d9e679580fea9c4c760d",
+    "url": "/React-To-do-App/index.html"
+  },
+  {
+    "revision": "19c092f3e6b434a236e8",
+    "url": "/React-To-do-App/static/css/main.bb625f17.chunk.css"
+  },
+  {
+    "revision": "9f4d0a5a4c76b9bbf6ae",
+    "url": "/React-To-do-App/static/js/2.f192c033.chunk.js"
+  },
+  {
+    "revision": "19c092f3e6b434a236e8",
+    "url": "/React-To-do-App/static/js/main.c7e489a3.chunk.js"
+  },
+  {
+    "revision": "58e544ef1f07f4befb37",
+    "url": "/React-To-do-App/static/js/runtime~main.20a52e4e.js"
+  }
+]);
---
+++ src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+
+class Todos extends Component {
+        render() {
+        // map though the props from the array "todos"
+        return this.props.todos.map((todo) => (
+            < TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete}
+            delTodo={this.props.delTodo} />
+        ));
+    }
+}
+
+// propTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+};
+
+export default Todos;
+
